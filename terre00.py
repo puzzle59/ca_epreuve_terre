a2e9f06a399dc150cf8491c125215aedacedaefc
puzzle59 <cimettamarvin@gmail.com>
2022-03-09 16:34:34 +0100

    Avec la correction

--- terre00.py
+++ terre00.py
@@ -1,9 +1,8 @@
 alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-alphabet_string=""
 for lettre in alphabet:
-    alphabet_string+=lettre
-print(alphabet_string)
+    print(lettre,end="")
 print("\n")
 #Problème 1: les lettres s'affichent lignes par lignes, je les veux en une seule
 #Idée : enlever l'option passage à la ligne de print ? Ce que j'ai fait pour le moment me semble
 #un peu artificiel
+#C'est bien ce que je pensais , avec l'option de print c'est mieux!
