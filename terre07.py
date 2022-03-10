c46f60c23b570c34da527acddb69824db0eaaf73
puzzle59 <cimettamarvin@gmail.com>
2022-03-10 10:55:15 +0100

    quid de l'argument entier

--- terre07.py
+++ terre07.py
@@ -1,19 +1,19 @@
 import sys
+
 if len(sys.argv)==2 :
 
     string=sys.argv[1]
-    print(string[0])
-    bool=True
-    for char in string:
-        if ord(char)>=48 and ord(char)<=57:
-            bool=False
-    if bool==True:
+
+    try:
+        b=int(string)
+        print('erreur')
+    except ValueError:
         compteur=0
         for char in string:
             compteur+=1
         print(compteur)
-    else:
-        print("erreur")
+    # else:
+    #     print("erreur")
 else:
     print("erreur")
 #questionnement sur le type string. quand on donne des chiffres en entrée cela
@@ -21,3 +21,9 @@ else:
 #table ascii , ord ?
 #autre idée: vérifier que l'argument commence par " ou ' et finit par "ou '
 #plus classe je pense .
+#autre solution avec cette tentative de int(). Mais ça n'empêche que pour
+# 50 cela doit mettre erreur , mais pour "50" cela devrait afficher 2..
+#tenté getopt , ça ne résout pas le pb. Argparse plutôt ?
+#
+#Apparemment les arguments en ligne de commande sont forcéments des strings,
+#la solution de tenter de l'utiliser en int semble donc pertinente
