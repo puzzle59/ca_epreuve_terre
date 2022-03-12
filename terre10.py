fb3ca487d7fe971bc70c16c76811226476b3f286
puzzle59 <cimettamarvin@gmail.com>
2022-03-12 23:45:48 +0100

    correction midi midnight

--- terre10.py
+++ terre10.py
@@ -4,20 +4,19 @@ if len(sys.argv[1:])==1:
     tf_hour=int(full_date[0:2])
     tf_minute=full_date[3:]
     ampm="AM"
-    if tf_hour == 00 and tf_minute==00:
-        hour_amp="00"
+    if tf_hour == 00 and tf_minute=="00":
+        hour_amp="12"
     elif tf_hour <=9:
         hour_amp="0"+str(tf_hour)
     elif tf_hour <12:
         hour_amp=tf_hours
-    elif tf_hour==12 and tf_minute==00:
-        hour_amp="00"
+    elif tf_hour==12 and tf_minute=="00":
+        hour_amp="12"
         ampm="PM"
     elif tf_hour <22:
         hour_amp="0"+str(tf_hour%12)
         ampm="PM"
     else:
-        hour_amp=tf_hour
         hour_amp=str(tf_hour%12)
         ampm="PM"
     print(f"{hour_amp}:{tf_minute}{ampm}")
