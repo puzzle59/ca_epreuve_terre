bbc9eca0bd48a61251094a1e2f70079720af7406
puzzle59 <cimettamarvin@gmail.com>
2022-03-12 19:40:57 +0100

    un peu plus light encore

--- terre10.py
+++ terre10.py
@@ -3,15 +3,13 @@ if len(sys.argv[1:])==1:
     full_date=sys.argv[1]
     tf_hour=int(full_date[0:2])
     tf_minute=full_date[3:]
+    ampm="AM"
     if tf_hour == 00 and tf_minute==00:
         hour_amp="00"
-        ampm="AM"
     elif tf_hour <=9:
         hour_amp="0"+str(tf_hour)
-        ampm="AM"
     elif tf_hour <12:
-        hour_amp=tf_hour
-        ampm="AM"
+        hour_amp=tf_hours
     elif tf_hour==12 and tf_minute==00:
         hour_amp="00"
         ampm="PM"
